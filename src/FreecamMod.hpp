17a2245d60e07cc67f762c715209382cd5cc04b9
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-20 13:09:14 +0700

    Update FreecamMod.hpp

--- src/FreecamMod.hpp
+++ src/FreecamMod.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "hooks/PacketHook.hpp"
+#include "hooks/PlayerHook.hpp"
 #include "ui/ModMenu.hpp"
 
 #include <pl/Mod.hpp>
@@ -14,21 +15,39 @@ public:
     FreecamMod();
 
     [[nodiscard]]
-    ll::mod::NativeMod& getSelf() const noexcept;
+    ll::mod::NativeMod&
+    getSelf() const noexcept;
 
     bool load();
+
     bool enable();
+
     bool disable();
+
     bool unload();
 
 private:
-    void setModuleEnabled(bool enabled);
-    void setCameraActive(bool active);
+    void setModuleEnabled(
+        bool enabled
+    );
 
-    ll::mod::NativeMod& mSelf;
+    void setCameraActive(
+        bool active
+    );
 
-    hooks::PacketHook mPacketHook;
-    ui::ModMenu mModMenu;
+    void restoreAndReset();
+
+    ll::mod::NativeMod&
+        mSelf;
+
+    hooks::PacketHook
+        mPacketHook;
+
+    hooks::PlayerHook
+        mPlayerHook;
+
+    ui::ModMenu
+        mModMenu;
 };
 
 } // namespace levifreecam
