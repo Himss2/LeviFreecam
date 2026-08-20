496abe0be85de3a245f55b08cbc17a18906348cd
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-20 12:37:51 +0700

    Update ModMenu.hpp

--- src/ui/ModMenu.hpp
+++ src/ui/ModMenu.hpp
@@ -7,16 +7,34 @@ namespace levifreecam::ui {
 
 class ModMenu final {
 public:
-    using ToggleCallback = std::function<void(bool)>;
+    using ModuleToggleCallback =
+        std::function<void(bool)>;
 
-    bool registerAll(std::string modId, ToggleCallback onToggle);
+    using CameraToggleCallback =
+        std::function<void(bool)>;
+
+    bool registerAll(
+        std::string modId,
+        ModuleToggleCallback onModuleToggle,
+        CameraToggleCallback onCameraToggle
+    );
+
     void unregisterAll() noexcept;
 
-    [[nodiscard]] bool registered() const noexcept { return mRegistered; }
+    [[nodiscard]]
+    bool registered() const noexcept {
+        return mRegistered;
+    }
 
 private:
     std::string mModId;
-    ToggleCallback mOnToggle;
+
+    ModuleToggleCallback
+        mOnModuleToggle;
+
+    CameraToggleCallback
+        mOnCameraToggle;
+
     bool mRegistered{false};
 };
 
