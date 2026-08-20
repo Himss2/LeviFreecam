835f99adc302e8ef3d33c5ac0fa0945839e7a6ea
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-21 06:31:52 +0700

    Update CameraState.hpp

--- src/camera/CameraState.hpp
+++ src/camera/CameraState.hpp
@@ -1,21 +1,43 @@
 #pragma once
 
-namespace levifreecam::camera
-{
+#include <atomic>
 
-struct CameraState
-{
-    bool enabled = false;
+namespace levifreecam::camera {
 
-    float x = 0.0f;
-    float y = 0.0f;
-    float z = 0.0f;
-
-    float yaw = 0.0f;
-    float pitch = 0.0f;
+struct Vec3 final {
+    float x{0.0f};
+    float y{0.0f};
+    float z{0.0f};
 };
 
+struct CameraOrientation final {
 
-CameraState& getCameraState();
+    float value[4]{
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f
+    };
+};
 
-}
+struct CameraState final {
+
+    std::atomic_bool
+        enabled{false};
+
+    std::atomic_bool
+        captureRequested{false};
+
+    std::atomic_bool
+        captured{false};
+
+    Vec3 position{};
+
+    CameraOrientation
+        orientation{};
+};
+
+CameraState&
+getCameraState() noexcept;
+
+} // namespace levifreecam::camera
