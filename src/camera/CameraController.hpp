44932c54735f493f9317ccfac8eb2d6b4b94bbcf
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-21 09:27:05 +0700

    Update CameraController.hpp

--- src/camera/CameraController.hpp
+++ src/camera/CameraController.hpp
@@ -4,52 +4,80 @@
 
 #include <cstddef>
 
+
 namespace levifreecam::camera {
+
 
 class CameraController final {
 
 public:
 
+
     static CameraController&
     instance() noexcept;
 
-    [[nodiscard]]
+
+
     bool readPosition(
         const void* cameraComponent,
         Vec3& out
     ) const noexcept;
+
+
 
     bool writePosition(
         void* cameraComponent,
         const Vec3& position
     ) const noexcept;
 
-    [[nodiscard]]
+
+
+    bool applyOffset(
+        void* cameraComponent,
+        const Vec3& offset
+    ) const noexcept;
+
+
+
     bool readOrientation(
         const void* cameraComponent,
         CameraOrientation& out
     ) const noexcept;
+
+
 
     bool writeOrientation(
         void* cameraComponent,
         const CameraOrientation& orientation
     ) const noexcept;
 
+
+
     static constexpr std::size_t
         kOrientationOffset =
             0x28;
+
+
 
     static constexpr std::size_t
         kPositionOffset =
             0x38;
 
+
+
     static constexpr std::size_t
         kCameraComponentStride =
             0x118;
 
+
+
 private:
 
+
     CameraController() = default;
+
+
 };
 
-} // namespace levifreecam::camera
+
+}
