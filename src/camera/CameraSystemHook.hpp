3bb95077db4cbe80bc76a116e56f14764890b237
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-22 14:48:31 +0700

    Update CameraSystemHook.hpp

--- src/camera/CameraSystemHook.hpp
+++ src/camera/CameraSystemHook.hpp
@@ -1,15 +1,41 @@
 #pragma once
 
+
 #include <cstdint>
+
 
 namespace levifreecam::camera {
 
-bool installCameraSystemHook() noexcept;
 
-void removeCameraSystemHook() noexcept;
 
-bool cameraSystemHookInstalled() noexcept;
+bool installCameraSystemHook()
+noexcept;
 
-std::uintptr_t cameraSystemAddress() noexcept;
+
+
+void removeCameraSystemHook()
+noexcept;
+
+
+
+bool cameraSystemHookInstalled()
+noexcept;
+
+
+
+std::uintptr_t cameraSystemAddress()
+noexcept;
+
+
+
+std::uintptr_t lastCameraSystem()
+noexcept;
+
+
+
+std::uintptr_t lastCameraRegistry()
+noexcept;
+
+
 
 }
